7cda733ab7112de7579cd07aee53f4d6c243687f
enrique <iqueart@yahoo.com>
2015-06-12 15:06:36 -0700

    allow you to add museums to the DB and list them on the index, also you can click list link to go to edit page

--- app.rb
+++ app.rb
@@ -17,6 +17,38 @@ end
 get("/museum/new") do
   erb(:add_museum)
 end
+
 get("/artwork/new") do
   erb(:add_art)
+end
+
+post("/") do
+  name = params.fetch("name")
+  museum = Museum.new({:name => name, :id => nil})
+  museum.save()
+  @museums = Museum.all
+  @artworks = Artwork.all
+  erb(:index)
+end
+
+# post("/") do
+#   description = params.fetch("description")
+#   artwork = Artwork.new({:description => description, :museum_id => nil})
+#   artwork.save()
+#   @museums = Museum.all
+#   @artworks = Artwork.all
+#   erb(:index)
+# end
+
+get("/museums/:id") do
+  @museum = Museum.find(params.fetch("id").to_i())
+  @artworks = Artwork.all
+  erb(:museum)
+end
+
+patch("/museums/:id") do
+  name = params.fetch("name")
+  @museum = Museum.find(params.fetch("id").to_i())
+  @museum.update({:name => name})
+  erb(:museum)
 end
